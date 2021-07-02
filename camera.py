3b1a825c1704a629db4bcc51f82c48bb756a86bd
Your Name <you@example.com>
2021-07-02 13:56:32 +0800

    add pull function

--- camera.py
+++ camera.py
@@ -1,21 +1,25 @@
 import time
 from datetime import datetime
 
-hz = 10
-fp = r'/sys/class/gpio/gpio321/value'
-
 def write_val(fp, val):
     with open(fp, 'w') as f:
         f.write(str(val))
 
-HIGH = True
-while True:
-    if HIGH:
-        write_val(fp, 1) # 1: pull down
-        HIGH = False
-    else:
-        write_val(fp, 0) # 1: pull up
-        HIGH = True
-        print(datetime.now(), 'pull up!')
-    time.sleep((1/hz)/2)
+def pull_up(fp):
+    write_val(fp, 1)
+    time.sleep(0.000001)
+    write_val(fp, 0) 
+
+if __name__ == '__main__':
+    hz = 1
+    fp = r'/sys/class/gpio/gpio321/value'
+
+    last_time = time.time()
+    while True:
+        pull_up(fp)
+        print(time.time(), "pull up")
+        while True:
+            if (time.time() - last_time) >= (1/hz):
+                last_time = time.time()
+                break
 
